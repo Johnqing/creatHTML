5b5dc1ae74be76fb384f7c671fc84c987a9e62ea
johnqing <csssnow@gmail.com>
2014-04-28 19:03:35 +0800

    fixed: info.json不为空时，报错的问题

--- add.js
+++ add.js
@@ -6,6 +6,17 @@ var NT = require('./lib/nt');
 var build = require('./lib/build');
 var config = require('./config');
 
+/*
+  * 检测对象是否是空对象(不包含任何可读属性)。
+  * 方法既检测对象本身的属性，也检测从原型继承的属性(因此没有使hasOwnProperty)。
+  */
+function isEmpty(obj){
+	for (var name in obj){
+		return false;
+	}
+	return true;
+};
+
 module.exports = function(reqData, key){
 
 	if(!config[key]) throw new Error('请在 config.js 内添加相应的信息！');
@@ -15,12 +26,21 @@ module.exports = function(reqData, key){
 	var curData = allData[key];
 
 	var tplData = {};
-	if(!curData.length && (!reqData.title && !reqData.id)){
-		console.log('init');
-		curData = [];
-		tplData[key] = null;
+
+	var objIsEmpty = isEmpty(reqData);
+
+	if(objIsEmpty){
+		if(!curData.length){
+			console.log('init');
+			tplData[key] = null;
+		}else{
+			tplData[key] = {
+				id: 0,
+				list: curData
+			}
+		}
 	}else{
-		if(!reqData.subtitle){
+		if(!reqData.subtitle && !curData.length){
 			console.log('not have subtitle');
 			var tArr = reqData.title.split('|');
 
